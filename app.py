9d1390de1a27aaf04b19b4a7826ad176559d8912
Sharan Thakur <sharandt19@live.com>
2024-04-30 21:47:30 +0530

    adds models options

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
-from rembg import remove
 import base64
+
+from rembg import remove, new_session
 
 from typing import Annotated
 from fastapi import FastAPI, UploadFile, Request, File, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 
 import uvicorn
 
+import os
+
+os.environ['U2NET_HOME'] = './models/'
+
+model_names = [
+    'u2net',
+    'u2net_human_seg',
+    'u2net_cloth_seg',
+    'isnet-general-use',
+]
+
+sessions = {
+    'u2net': new_session(model_name=model_names[0]),
+}
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def health(request: Request):
     return templates.TemplateResponse('dynamic.html', { "request": request })
 
-@app.post("/remove")
-def remove_bg(request: Request, file: Annotated[UploadFile, File()], mask_only: Annotated[str, Form()] = 'off'):
-    try:
-        only_mask = mask_only == 'on'
-        data = file.file.read()
 
-        output_array = remove(data, only_mask=only_mask)
+@app.post("/remove")
+async def remove_bg(
+    request: Request,
+    file: Annotated[UploadFile, File()],
+    mask_only: Annotated[str, Form()] = 'off',
+    name_of_model: Annotated[str, Form()] = 'u2net'
+) -> HTMLResponse:
+    try:
+        if name_of_model not in sessions.keys():
+            sessions[name_of_model] = new_session(model_name=name_of_model)
+        
+        current_session = sessions[name_of_model]
+
+        only_mask = mask_only == 'on'
+        data = await file.file.read()
+
+        output_array = remove(data, only_mask=only_mask, session=current_session)
 
         output_img = base64.b64encode(output_array).decode('utf-8')
 
@@ -38,7 +66,7 @@ def remove_bg(request: Request, file: Annotated[UploadFile, File()], mask_only: 
             }
         )
     except:
-        return RedirectResponse('/')
+        return RedirectResponse("/", status_code=303)
 
 @app.get("/remove")
 def remove_bg_redirect():
